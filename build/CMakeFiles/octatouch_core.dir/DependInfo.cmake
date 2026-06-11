b6973b624534f00f9134d394a1723c8541d53a46
Deonte Watts <284501602+DeontewattsV1@users.noreply.github.com>
2026-06-11 03:50:00 -0700

    feat: initial commit — source files

---
+++ build/CMakeFiles/octatouch_core.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/data/octatouch-sdk-core/core/src/FingerTracker.cpp" "CMakeFiles/octatouch_core.dir/core/src/FingerTracker.cpp.o" "gcc" "CMakeFiles/octatouch_core.dir/core/src/FingerTracker.cpp.o.d"
+  "/mnt/data/octatouch-sdk-core/core/src/GestureEngine.cpp" "CMakeFiles/octatouch_core.dir/core/src/GestureEngine.cpp.o" "gcc" "CMakeFiles/octatouch_core.dir/core/src/GestureEngine.cpp.o.d"
+  "/mnt/data/octatouch-sdk-core/core/src/GestureRecognizer.cpp" "CMakeFiles/octatouch_core.dir/core/src/GestureRecognizer.cpp.o" "gcc" "CMakeFiles/octatouch_core.dir/core/src/GestureRecognizer.cpp.o.d"
+  "/mnt/data/octatouch-sdk-core/core/src/IntentResolver.cpp" "CMakeFiles/octatouch_core.dir/core/src/IntentResolver.cpp.o" "gcc" "CMakeFiles/octatouch_core.dir/core/src/IntentResolver.cpp.o.d"
+  "/mnt/data/octatouch-sdk-core/core/src/Types.cpp" "CMakeFiles/octatouch_core.dir/core/src/Types.cpp.o" "gcc" "CMakeFiles/octatouch_core.dir/core/src/Types.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
